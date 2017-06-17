c6ab2dae6b0897162fad97b88d7a4e6586630ee5
Mikhail Plavskiy <mikhail.plavskiy@quest.com>
2017-06-17 13:08:50 +0300

    scale either up or down the deployment

---
+++ scale.sh
@@ -0,0 +1,9 @@
+NS=$1
+DEPL=$2
+RS=$3
+
+deployment --namespace=$NS \
+    --no-headers |
+    awk '{print $1}' |
+    grep $DEPL |
+    xargs kubectl scale deployment --replicas=$RS --namespace=$NS
