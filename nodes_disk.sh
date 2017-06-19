f3237db38c9bda99e493fcdb9545239e6d051adf
Mikhail Plavskiy <mikhail.plavskiy@quest.com>
2017-06-19 22:28:15 +0300

    Update nodes_disk.sh

--- nodes_disk.sh
+++ nodes_disk.sh
@@ -2,7 +2,7 @@ NODES=`kubectl get nodes --no-headers | awk '{print $1}'`
 
 for n in $NODES;do
    echo $n
-   ssh $n df -h | grep sda
-   ssh $n df -i | grep sda
+   ssh -oStrictHostKeyChecking=no $n df -h | grep sda
+   ssh -oStrictHostKeyChecking=no $n df -i | grep sda
 done
 
